f7cc0c50617bfb52757e80ca4d4caa06e4a65e88
Your Name <you@example.com>
2021-12-14 13:22:22 +0100

    project final

--- Project/weatherStation/weatherStation/weatherStation/main.c
+++ Project/weatherStation/weatherStation/weatherStation/main.c
@@ -1,16 +1,18 @@
 /***********************************************************************
-
+WEATHER STATION:
+Temperature, humidity and light intensity sensors (as we don't have 
+those in the lab we use a photoresistor in a voltage divider) to get 
+the light intensity. Then we show all these values in the LCD.
+The XY motors of the weather station moves according to the light
+intensity, tracking the sun
  **********************************************************************/
 
 /* Defines -----------------------------------------------------------*/
-#define MOTOR1   PB5    // AVR pin where motor is connected
-#define MOTOR2   PB4    // AVR pin where motor is connected
+#define MOTOR1   PB5    // AVR pin where motor 1 is connected
+#define MOTOR2   PB4    // AVR pin where motor 2 is connected
 #ifndef F_CPU
 # define F_CPU 16000000  // CPU frequency in Hz required for UART_BAUD_SELECT
 #endif
-#define DELAY_1 1
-#define DELAY_2 2
-#define DELAY_15 1.5
 
 
 /* Includes ----------------------------------------------------------*/
@@ -34,7 +36,7 @@ typedef enum {              // FSM declaration
 /* Function definitions ----------------------------------------------*/
 /**********************************************************************
  * Function: Main function where the program execution begins
- * Purpose:  Use Timer/Counter1 and send I2C (TWI) address every 33 ms.
+ * Purpose:  Use Timer/Counter1 and send I2C (TWI) address every 262 ms.
  *           Send information about scanning process to UART.
  * Returns:  none
  **********************************************************************/
@@ -51,15 +53,9 @@ int main(void)
     TIM1_overflow_262ms();
     TIM1_overflow_interrupt_enable();
 
-    // Configure 16-bit Timer/Counter2 to update FSM
-    // Set prescaler to 16 ms and enable interrupt for the servomotor
-    TIM2_overflow_16ms();
-    TIM2_overflow_interrupt_enable();
-
     // Enables interrupts by setting the global interrupt mask
     sei();
 
-    
     // Put strings to ringbuffer for transmitting via UART
     uart_puts("\r\nScan I2C-bus for devices:\r\n");
     // Initialize UART to asynchronous, 8N1, 9600
@@ -78,11 +74,8 @@ int main(void)
     // Set clock prescaler to 128    
     ADCSRA |= (1<<ADPS2) | (1<<ADPS1) |(1<<ADPS0) ;
     
-    
-    
-
 //----------------------------------------------------
-    // MOTOR at port B
+    // Initialize and set to low motors at port B
     GPIO_config_output(&DDRB, MOTOR1);
     GPIO_write_low(&PORTB, MOTOR1);
     GPIO_config_output(&DDRB, MOTOR2);
@@ -91,10 +84,8 @@ int main(void)
     // Infinite loop
     while (1)
     {
-
-
+		
     }
-
     // Will never reach this
     return 0;
 }
@@ -105,14 +96,14 @@ int main(void)
 /**********************************************************************
  * Function: Timer/Counter1 overflow interrupt
  * Purpose:  Update Finite State Machine and test I2C slave addresses 
- *           between 8 and 119.
+ *           between 8 and 119 to read sensors values.
  **********************************************************************/
 ISR(TIMER1_OVF_vect)
 {
-    static state_t state = STATE_SEND;  // Current state of the FSM
+    static state_t state = STATE_SEND;     // Current state of the FSM
     static uint8_t addr = 0x5c;            // I2C slave address
-    uint8_t result = 1;                 // ACK result from the bus
-    char uart_string[] = "0000"; // String for converting numbers by itoa()
+    uint8_t result = 1;                    // ACK result from the bus
+    char uart_string[] = "0000";           // String for converting numbers by itoa()
     static uint8_t number_of_devices=0;
     uint8_t temp_int;
     uint8_t temp_frac;
@@ -122,57 +113,50 @@ ISR(TIMER1_OVF_vect)
     
     // Start ADC conversion
     ADCSRA |= (1<<ADSC);
-
-    uart_puts("Reading \r\n");
     
     //Read temperature     
     result = twi_start((addr<<1) + TWI_WRITE);
-    twi_write(0x02);
+    twi_write(0x02);                              //Register 0x02 and 0x03 stores temperature values (integer and fractional part)
     result = twi_start((addr<<1) + TWI_READ);
     
     temp_int=twi_read_ack();
     temp_frac=twi_read_nack();
     twi_stop();
-    lcd_init(LCD_DISP_ON);
+	
+    lcd_init(LCD_DISP_ON);		    // LCD is ON (without cursor)
     lcd_gotoxy(0,0);
     lcd_puts("Tem: ");
-    itoa(temp_int,lcd_string,10); // temp integer part
+    itoa(temp_int,lcd_string,10);   // temperature integer part
     lcd_puts(lcd_string);
-    lcd_puts(".");
-    itoa(temp_frac,lcd_string,10); // temp fractional part
+    lcd_puts(".");					// Decimal point
+    itoa(temp_frac,lcd_string,10);  // temperature fractional part
     lcd_puts(lcd_string);
-    lcd_puts("C");
+    lcd_puts("C");					// Celsius degrees
 //----------------------------------------------
-     //Read hum
+     //Read humidity
      result = twi_start((addr<<1) + TWI_WRITE);
-     twi_write(0x00);
+     twi_write(0x00);                              //Register 0x00 and 0x01 stores himidity values (integer and fractional part)
      result = twi_start((addr<<1) + TWI_READ);
 
      hum_int=twi_read_ack();
      hum_frac=twi_read_nack();
      twi_stop();
+	 
      lcd_gotoxy(0,1);
      lcd_puts("Hum: ");
-     itoa(hum_int,lcd_string,10); // hum integer part
+     itoa(hum_int,lcd_string,10);  // hum integer part
      lcd_puts(lcd_string);
      lcd_puts(".");
      itoa(hum_frac,lcd_string,10); // hum fractional part
      lcd_puts(lcd_string);
-     lcd_puts("%");
+     lcd_puts("%");                // Percentage of humidiry
 }
-/**
-ISR(TIMER2_OVF_vect){
-        GPIO_toggle(&DDRB, MOTOR);
-        _delay_ms(DELAY_15);
-        GPIO_toggle(&DDRB, MOTOR);
-        _delay_ms(DELAY_15);
 
-}
-*/
 
 /**********************************************************************
  * Function: ADC complete interrupt
- * Purpose:  Display value on LCD and send it to UART.
+ * Purpose:  Read voltage value (light intensity value) from the 
+ * photoresistor and move servo motors according to this value.
  **********************************************************************/
 ISR(ADC_vect)
 {
@@ -181,14 +165,15 @@ ISR(ADC_vect)
     char lcd_string[4] = "0000";
     value=ADC;
     
-    //Put new value TO LCD
-    itoa(value, lcd_string, 10);  // Convert decimal value to string
+    //Put light intensity value TO LCD
+    itoa(value, lcd_string, 10);  // Convert decimal value (light intensity) to string
     lcd_gotoxy(12,0);
     lcd_puts(lcd_string);
     // send the same value to UART
      uart_puts(lcd_string);
      uart_puts(" ");
     
+	//If light intensity is below 300, horizontal motor moves to the left
     if(value>=0 && value<=300)
     {
         GPIO_write_high(&PORTB, MOTOR1);   
@@ -196,6 +181,7 @@ ISR(ADC_vect)
         GPIO_write_low(&PORTB, MOTOR1) ;
         _delay_ms(19);
     }    
+	//If light intensity is between 300 and 600, horizontal motor moves to the middle (front)
      else if(value>300 && value <=600)
      {
         GPIO_write_high(&PORTB, MOTOR1);
@@ -203,6 +189,7 @@ ISR(ADC_vect)
         GPIO_write_low(&PORTB, MOTOR1) ;
         _delay_ms(18.5);         
      }
+	 //If light intensity is over 600, horizontal motor moves to the right
      else
      {
         GPIO_write_high(&PORTB, MOTOR1);
@@ -210,7 +197,7 @@ ISR(ADC_vect)
         GPIO_write_low(&PORTB, MOTOR1) ;
         _delay_ms(18);         
      }
-     
+     //If light intensity is below 500, vertical motor moves up
      if(value>0 && value <=500)
      {
          GPIO_write_high(&PORTB, MOTOR2);
@@ -218,6 +205,7 @@ ISR(ADC_vect)
          GPIO_write_low(&PORTB, MOTOR2) ;
          _delay_ms(18.5);
      }
+     //If light intensity is over 500, vertical motor moves down
      else
      {
          GPIO_write_high(&PORTB, MOTOR2);
@@ -227,4 +215,3 @@ ISR(ADC_vect)
      }
      
 }   
-
